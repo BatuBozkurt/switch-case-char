a6a48aa0b52edfcbfe96ca393975dccad8915b0b
sudenazcivelek <sudenazzcivelek@gmail.com>
2023-06-23 14:13:27 +0300

    Add project files.

---
+++ Unlu-Unsuz switch case char/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Bir harf giriniz:  ");
+char harf = char.Parse(Console.ReadLine().ToLower());
+switch (harf)
+{
+    case 'a':
+    case 'e':
+    case 'ı':
+    case 'i':
+    case 'u':
+    case 'ü':
+    case 'o':
+    case 'ö':
+        Console.WriteLine("ünlü harf"); break;
+        default: Console.WriteLine("Ünsüz harf"); break;
+}
